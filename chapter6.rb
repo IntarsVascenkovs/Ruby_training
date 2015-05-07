ee69074b99aa2888445afbf2c474e28a97879357
Intars Vascenkovs <intars.vascenkovs@diatomenterprises.com>
2015-05-07 19:37:55 +0300

    class methods

--- chapter6.rb
+++ chapter6.rb
@@ -10,6 +10,16 @@ class Animal
 	attr_writer :color
 	attr_reader :legs, :arms
 
+	def self.all_species
+		['cat', 'dog', 'pig', 'horse']
+	end
+
+	def initialize(noise, legs=5, arms=3.2)
+		@noise = noise
+		@legs = legs
+		@arms = arms
+	end
+
 	def color
 		"The color is #{@color}"
 	end
@@ -23,11 +33,13 @@ end
 
 #puts atlaide = Product.new.atlaide(90, 20)
 
-animal1 = Animal.new
-animal1.name = "vaardins"
+puts Animal.all_species
+
+animal1 = Animal.new("Buuu", 4)
+animal1.name = "\nvaardins"
 animal1.color = "blue"
-animal1.legs = 4
 
 puts animal1.name
 puts animal1.color
 puts animal1.legs
+puts animal1.arms
