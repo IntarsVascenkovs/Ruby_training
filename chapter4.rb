693040a3c9d2f4247e1c0699de57ff99cc2edf5b
Intars Vascenkovs <intars.vascenkovs@diatomenterprises.com>
2015-05-07 12:54:48 +0300

    hashes merge

--- chapter4.rb
+++ chapter4.rb
@@ -1,2 +1,6 @@
-a = [*1..10].delete_if{|i| i %3 ==0}
-puts a
+#a = [*1..10].delete_if{|i| i %3 ==0}
+#puts a
+
+h1 = {"a"=>111, "b"=>222}
+h2 = {"b"=>333, "c"=>444}
+puts h1.merge(h2) {|key, old, new| old + new + 111}
