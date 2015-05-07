3df7cc248bf27dd2cd078b894c1dbf77a9394b71
Intars Vascenkovs <intars.vascenkovs@diatomenterprises.com>
2015-05-07 19:18:16 +0300

    reader and writer

--- chapter6.rb
+++ chapter6.rb
@@ -5,6 +5,29 @@ class Product
 	end
 end
 
+class Animal
+	attr_accessor :name
+	attr_writer :color
+	attr_reader :legs, :arms
+
+	def color
+		"The color is #{@color}"
+	end
+
+	def legs=(kaaju_skaits)
+		@legs ="Kāju skaits ir: #{kaaju_skaits}"
+	end
+end
+
 #produkts = Product.new
 
-puts atlaide = Product.new.atlaide(90, 20)
+#puts atlaide = Product.new.atlaide(90, 20)
+
+animal1 = Animal.new
+animal1.name = "vaardins"
+animal1.color = "blue"
+animal1.legs = 4
+
+puts animal1.name
+puts animal1.color
+puts animal1.legs
