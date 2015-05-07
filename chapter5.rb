2a985a19c72f3ee7c0d00fe05dc27d644ec310d7
Intars Vascenkovs <intars.vascenkovs@diatomenterprises.com>
2015-05-07 17:54:59 +0300

    return

--- chapter5.rb
+++ chapter5.rb
@@ -4,12 +4,12 @@ end
 
 @word = ["banana", "appleeeeeee", "orange", "pear"]
 
-def longest_word(word)
+def longest_word(word = ['aaa', 'bb'])
 	
 	longest_word = word.inject do |memo, word|
 		memo.length > word.length ? memo : word
 	end
-	puts longest_word
+	return puts longest_word
 end
 
 def over_five?
@@ -17,5 +17,16 @@ def over_five?
 	puts value > 5 ? 'ir' : 'nav'
 end
 
-longest_word(@word)
-longest_word(@word.map {|a| a.reverse})
+def two_returned_values(a, b)
+	c = "summa ir:"
+	return [c, a+b]
+end
+
+puts two_returned_values(3, 5)
+puts "----------"
+a, b = two_returned_values(6, 7)
+puts "#{a} EUR #{b}"
+
+#longest_word(@word)
+#longest_word(@word.map {|a| a.reverse})
+#longest_word
