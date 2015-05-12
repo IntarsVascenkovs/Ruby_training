9cb6aa08a2a095a1a0535e1d2999fbcf76314f5a
Intars Vascenkovs <intars.vascenkovs@diatomenterprises.com>
2015-05-12 19:27:28 +0300

    end of chapter 7

---
+++ file_location.rb
@@ -0,0 +1,9 @@
+ puts File.join('', 'Users', 'intars.vascenkovs', 'Documents', 'GitHub', 'Ruby_training')
+
+ puts __FILE__
+
+ puts File.expand_path(__FILE__)
+
+ puts File.dirname(__FILE__)
+
+ puts File.join(File.dirname(__FILE__), '..')
